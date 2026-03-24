b76d22933dde57c394841e156191d19b7d40d83d
kleegr <Info@kleegr.com>
2026-03-24 13:17:58 -0400

    Delete src/app/(public)/jobs/[slug]/apply/page.tsx

--- src/app/(public)/jobs/[slug]/apply/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file should be deleted - it conflicts with [id] route
-// Keeping it as a no-op to avoid build errors until manually deleted from GitHub
-// TO FIX: Delete the entire src/app/(public)/jobs/[slug] folder from GitHub
